3fb4ccf81c289bfc6fc3ca58f7ab38bf67a6fd89
JuanGutierrez205 <jdgutierrezm@unicauca.edu.co>
2025-03-08 11:08:56 -0500

    Database2.js

--- src/db.js
+++ src/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const DB_USER = "admin"; // Usuario de MongoDB
 const DB_PASSWORD = "password123"; // Contraseña del usuario
-const DB_HOST = "192.168.0.110"; // O IP del servidor MongoDB
+const DB_HOST = "192.168.1.11"; // O IP del servidor MongoDB
 const DB_PORT = "28017"; // Puerto en el que corre MongoDB
 const DB_NAME = "appdb"; // Nombre de la base de datos
 
